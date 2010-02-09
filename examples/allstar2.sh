c64b5fe2e5052d0e5521b88b6e5c2ef1e50a48af
Sébastien Fabbro <sfabbro@uvic.ca>
2010-02-09 13:13:54 -0800

    New recipe

---
+++ examples/allstar2.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+# get_psf_fwhm <psf file>
+get_psf_fwhm() {
+    awk 'NR==2 {print ($1+$2)*1.176; exit}' ${1} 2> /dev/null
+}
+
+make_psf_als() {
+    local im=$(basename ${1%.*})
+    # 1) make psf with high S/N stars
+    allphot daophot opt --dict=${dict} ${1}
+    allphot daophot find --option TH=10 ${1}
+    allphot daophot phot ${1}
+    allphot daophot pick --nstars=50 --magfaint=13 ${im}.ap
+    allphot daophot psf \
+	--option VA=-1 \
+	--option AN=1 \
+	${1}
+    #allphot filters neighbours ${im}.{nei,lst}
+    # 2) subtract stars neighbours and rebuild
+    allphot allstar do \
+	--option WA=0 \
+	--option IS=0 \
+	--option OS=0 \
+	${1}
+    allphot daophot sort --index=3 ${im}.als
+    allphot daophot substar --in=${im}.srt --keep=${im}.lst ${1}
+    allphot daophot psf \
+	--option VA=-1 \
+	--option AN=1 \
+	--pho=${im}.ap \
+	--in=${im}.lst \
+	--nei=${im}.nei \
+	--out=${im}.psf \
+	${im}s.fits
+    allphot allstar do ${1}
+    # 3) detect all the stars now
+    fwhm=$(get_psf_fwhm ${im}.psf)
+    allphot daophot opt \
+	--dict=${dict} \
+	--option AN=-6 \
+	--option FI=${fwhm} \
+	--option PS="$(echo ${fwhm} | awk '{print $1*4}')" \
+	--option TH=4 \
+	--out=${im}.opt \
+	${1}
+    allphot daophot find --opt=${im}.opt ${im}s.fits
+
+    off=$(tail -n 1 ${im}.coo | awk '{print $1+1}')
+    allphot daophot offset --id=${off} --out=${im}s.off ${im}s.coo
+    allphot daophot phot --in=${im}s.off ${im}s.fits
+    allphot allstar do --psf=${im}.psf --in=${im}s.ap ${im}s.fits
+    allphot daophot append --out=${im}.cmb ${im}s.als ${im}.als
+    allphot daophot sort --index=4 --out=${im}.srt ${im}.cmb
+    allphot daophot phot --radius A2=0 --in=${im}.srt ${1}
+    allphot daophot pick --magfaint=15 --nstars=200 ${im}.ap
+    allphot daophot substar --in=${im}.ap --keep=${im}.lst ${1}
+    allphot daophot psf --in=${im}.lst --pho=${im}.ap --out=${im}.psf --nei=${im}.nei ${im}s.fits
+    allphot allstar do 	\
+	--option FI=${fwhm} \
+	--option IS="$(echo ${fwhm} | awk '{print $1*0.7}')" \
+	--option OS="$(echo ${fwhm} | awk '{print $1*4}')" \
+	--in=${im}.ap ${1}
+
+    allphot daophot find --option TH=2.5 ${im}s.fits
+    off=$(tail -n 1 ${im}.als | awk '{print $1+1}')
+    allphot daophot offset --id=${off} --out=${im}s.off ${im}s.coo
+    allphot daophot phot --in=${im}s.off ${im}s.fits
+    allphot allstar do --psf=${im}.psf --in=${im}s.ap ${im}s.fits
+    allphot daophot append --out=${im}.cmb ${im}s.als ${im}.als
+    allphot daophot sort --index=4 --out=${im}.srt ${im}.cmb
+    allphot daophot phot --radius A2=0 --in=${im}.srt ${1}
+    allphot daophot pick --magfaint=15 --nstars=200 ${im}.ap
+    allphot daophot substar --in=${im}.ap --keep=${im}.lst ${1}
+    allphot daophot psf --in=${im}.lst --pho=${im}.ap --out=${im}.psf --nei=${im}.nei ${im}s.fits
+    allphot filters neighbours sky00.nei sky00.lst
+    allphot daophot substar --in=${im}.ap --keep=${im}.lst ${1}
+    allphot daophot psf --in=${im}.lst --pho=${im}.ap --out=${im}.psf --nei=${im}.nei ${im}s.fits
+    allphot filters neighbours sky00.nei sky00.lst
+    allphot daophot substar --in=${im}.ap --keep=${im}.lst ${1}
+    allphot daophot psf --in=${im}.lst --pho=${im}.ap --out=${im}.psf --nei=${im}.nei ${im}s.fits
+    allphot allstar do --in=${im}.ap ${1}
+}
+
+
+dict=sky.dict
+
+for i in $@; do
+    make_psf_als ${i}
+done
