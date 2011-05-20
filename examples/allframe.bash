731f8a1ef6f462027afa082b500e3d6b3098d1d5
Sebastien Fabbro <sfabbro@uvic.ca>
2011-05-20 10:40:11 -0700

    cleaned-up examples

---
+++ examples/allframe.bash
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ $# -lt 1 ]; then
+    echo "Usage: $(basename $0) <file1.als>...<file m.als>"
+    echo " First one is reference"
+fi
+
+allphot daomatch do --out=${1%.*}.mch $*
+allphot daomaster do sky.mch
+allphot allframe do sky.mch
+
---
+++ examples/allstar.bash
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+do_psf_allstar() {
+    local options
+    local gain=$(fitskey -n -p GAIN ${1})
+    [[ ${gain} == absent ]] || options="--option GA=${gain}"
+    local ron=$(fitskey -n -p RDNOISE ${1})
+    [[ ${ron} == absent ]] || options="--option RE=${ron}"
+    allphot daophot opt "${options}"
+    allphot daophot find ${1}
+    allphot daophot phot ${1}
+    allphot daophot pick ${1%.*}.ap
+    local i
+    for i in $(seq 3); do
+	allphot cat neighbours ${1%.*}.{nei,lst}
+	allphot daophot psf ${1}
+    done
+    allphot allstar ${1}
+}
+
+if [ $# -lt 2 ]; then
+    echo "Usage: $(basename $0) <fits image 1>...<fits image n>"
+fi
+
+for im in *@; do
+    do_psf_allstar ${im}
+done
