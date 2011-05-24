0b9f931d080b1e09e4431b8fff500b3b996c306b
Sebastien Fabbro <sfabbro@uvic.ca>
2011-05-24 12:14:34 -0700

    fixed recipe example

--- examples/daorecipe.bash
+++ examples/daorecipe.bash
@@ -1,6 +1,11 @@
 daophot_opt GA=1.5 RE=3
 daophot_attach ${1}
-daophot_find 1,1, ${1}
+daophot_find 1,1 ${1%.*}.coo
+cat > photo.opt <<EOF
+A1=4
+IS=12
+OS=20
+EOF
 daophot_phot photo.opt ${1%.*}.coo ${1%.*}.ap
 daophot_pick ${1%.*}.ap 30 15 ${1%.*}.lst
 daophot_psf ${1%.*}.ap ${1%.*}.lst ${1%.*}.psf
